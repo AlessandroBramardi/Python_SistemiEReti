4ddb52334b17c30634eab7a9f5aa151cbfd1d7d6
AlessandroBramardi <alessandro.bramardi@itiscuneo.eu>
2023-10-23 11:23:40 +0200

    Correzioni
    Correzione degli esercizi di compito

--- pag73_18.py
+++ pag73_18.py
@@ -1,7 +1,10 @@
+import math 
 def main():
     n=200
-    lista_quad_disp=[i*i for i in range(n) if i%2!=0]
+    lista=[i**2 for i in range(0, 1000) if (i%2!=0) and (i**2<n)]
+    ##lista_quad_disp=[i**2 for i in range(0, int(math.sqrt(n))+1) if i%2!=0]
     print("Caricamento con list comprehension quadrati perfetti dispari:")
-    print(lista_quad_disp)
+    print(lista)
+    ##print (lista_quad_disp)
 if __name__ == "__main__":
     main()
--- pag73_21.py
+++ pag73_21.py
@@ -1,6 +1,6 @@
 def main():
     parola="ciao come stai?"
-    parola_consonanti=''.join([c for c in parola if c not in "aeiou"])
+    parola_consonanti="".join([c for c in parola if c not in "aeiou"])
     print(parola)
     print(parola_consonanti)
 if __name__ == "__main__":
